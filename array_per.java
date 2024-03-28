3e30472ccbb2f6dfc1eb9ed806be9c37f8b7e8ce
VaidehiC0303 <165248644+VaidehiC0303@users.noreply.github.com>
2024-03-28 11:42:46 +0530

    Add files via upload

---
+++ array_per.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class array_per {
+    // leetcode 46
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        <ArrayList<Integer>> temp = new ArrayList<>();
+    }
+    public static void call(int nums ,ArrayList<ArrayList<Integer>> ans,int i,<ArrayList<Integer>> temp ) {
+        for(i=0;i<nums.length;i++){
+            temp.add(nums[i]);
+            if(nums[i]==temp.contains(num[i])){
+                return;
+            }
+            else {
+                temp.add(nums[i]);
+            }
+
+        }
+        if ()
+    }
+}
